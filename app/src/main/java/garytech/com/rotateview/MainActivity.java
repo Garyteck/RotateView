a0311f491da496df7e8e0b9f8b91b7908ac74ca4
Garyteck <garytech@hotmail.fr>
2016-06-04 09:00:35 +0200

    change image resource

--- app/src/main/java/garytech/com/rotateview/MainActivity.java
+++ app/src/main/java/garytech/com/rotateview/MainActivity.java
@@ -3,18 +3,28 @@ package garytech.com.rotateview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Main activity
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RotateView.Listener {
 
-    private ImageView mRotateView;
+    private RotateView mRotateView;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRotateView = (ImageView) findViewById(R.id.main_rotate_view);
+        mRotateView = (RotateView) findViewById(R.id.main_rotate_view);
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        mRotateView.setListener(this);
+    }
+
+    @Override
+    public void onAngleChanged(float angle) {
+        mTextView.setText(""+angle);
     }
 }
--- app/src/main/java/garytech/com/rotateview/RotateView.java
+++ app/src/main/java/garytech/com/rotateview/RotateView.java
@@ -7,23 +7,40 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
 /**
- * Created by gary on 05/05/16.
+ * Imageview that is divided in 4 quadrants ( NE,NW,SE,SW)
+ * this Imageview can rotate around its center according to the user click/swipe
  */
 public class RotateView extends ImageView {
 
-    private static final String TAG = "RotateView";
     /**
-     * Matrix to handle image rotation
+     * Cache value of the current angle
+     */
+    private double mCurrentAngle = 0;
+
+    /**
+     * Position of the ImageView in the screen
+     */
+    private int[] mLocation = new int[2];
+
+    /**
+     * Matrix representing the image, it is used to perform the rotation
      */
     private Matrix mMatrix;
 
+    private Listener mListener;
+
+    public interface Listener {
+        void onAngleChanged(float angle);
+    }
 
     public RotateView(Context context) {
         super(context);
         initializeView();
+
     }
 
     public RotateView(Context context, AttributeSet attrs) {
@@ -35,49 +52,124 @@ public class RotateView extends ImageView {
     public RotateView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initializeView();
-
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public RotateView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         initializeView();
-
     }
 
+    @Override public boolean onTouchEvent(final MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                rotateView(getAngle(translateX(event.getRawX()), translateY(event.getRawY())));
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return true;
+    }
+
+    public void setListener(Listener listener) {
+        mListener = listener;
+    }
 
     private void initializeView() {
+        // initialisation parameters
+
         if (mMatrix == null) {
             mMatrix = new Matrix();
         }
 
-        setImageMatrix(mMatrix);
+        setScaleType(ScaleType.MATRIX);
+
+        // calculate the view position once the layout is setup
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                getLocationOnScreen(mLocation);
+
+            }
+        });
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "Raw x "+event.getRawX()+" Raw y"+event.getRawY());
-                Log.d(TAG, " x "+event.getX()+" y"+event.getY());
-                rotateView((float)calculateAngle(event.getX(),event.getY()));
-                return true;
+    /**
+     * rotate the compass at the given value
+     * @param newAngle the angle of the angle with the horizontal axis
+     */
+    public void rotateView(double newAngle) {
+        if (mListener != null) {
+            mListener.onAngleChanged((float)newAngle);
         }
-        return super.onTouchEvent(event);
 
+        // rotate view
+        mMatrix.postRotate((float) (mCurrentAngle - newAngle), getWidth() / 2, getHeight() / 2);
+        setImageMatrix(mMatrix);
+        Log.w("rotateView", mMatrix.toShortString());
+
+        mCurrentAngle = newAngle;
     }
 
+    /**
+     * translate the x position relative to the center of the ImageView
+     *
+     * @param x the raw x value
+     * @return the translated x
+     */
     private float translateX(float x) {
-        return x;
+        return x-(mLocation[0]+getWidth()/2);
     }
 
-    private double calculateAngle(float x, float y) {
-        return Math.toDegrees((float) Math.atan2(y, x));
+    /**
+     * translate the y position relative to the  center of the ImageView
+     *
+     * @param y the raw x value
+     * @return the translated y
+     */
+    private float translateY(float y) {
+        return -(y-(mLocation[1]+getHeight()/2));
     }
-    private void rotateView(float angle) {
-        Log.d(TAG, "rotateView: "+angle);
-        mMatrix.postRotate(angle,getX()+getWidth()/2,getY()+getHeight()/2);
-        invalidate();
+
+
+    /**
+     *
+     * @param x the x position translated in the image axis
+     * @param y the y position translate in the image axis
+     * @return the quadrant that was touched (NE,NW,SE,SW ) == > (1,2,3,4)
+     */
+    private static int getQuadrant(double x, double y) {
+        if (x >= 0) {
+            return (y >= 0) ? 1 : 4;
+        } else {
+            return (y >= 0) ? 2 : 3;
+        }
     }
+
+    /**
+     * return the angle in degrees
+     *
+     * @param x the x position
+     * @param y the y position
+     * @return angle
+     */
+    private double getAngle(double x, double y) {
+
+        switch (getQuadrant(x, y)) {
+            case 1:
+                return (Math.asin(y / Math.hypot(x, y)) * 180) / Math.PI;
+            case 2:
+                return 180 - ((Math.asin(y / Math.hypot(x, y)) * 180) / Math.PI);
+            case 3:
+                return 180 + ((-1 * Math.asin(y / Math.hypot(x, y)) * 180) / Math.PI);
+            case 4:
+                return 360 + ((Math.asin(y / Math.hypot(x, y)) * 180) / Math.PI);
+            default:
+                return 0;
+        }
+    }
+
+
 }
